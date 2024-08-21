7b06d2e19efc39a9b31a0de79fbf947d95ac4606
Vladislav <44534111+Vladik356@users.noreply.github.com>
2024-08-21 22:12:14 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,10 @@ dependencies {
     testImplementation ("org.junit.jupiter:junit-jupiter-api:5.11.0")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.11.0")
     testImplementation ("org.testng:testng:7.10.2")
+    testImplementation ("ch.qos.logback:logback-classic:1.2.11")
 }
 
 tasks.test {
     useJUnitPlatform()
+    useTestNG()
 }
