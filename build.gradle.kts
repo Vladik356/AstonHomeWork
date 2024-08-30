662d9c037c31750ea77a7a767d2aee2d2d60103a
Vladislav <44534111+Vladik356@users.noreply.github.com>
2024-08-30 17:38:40 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation ("io.github.bonigarcia:webdrivermanager:5.3.2")
     testImplementation ("org.junit.jupiter:junit-jupiter-api:5.9.3")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.9.3")
     implementation ("org.seleniumhq.selenium:selenium-java:4.8.0")
