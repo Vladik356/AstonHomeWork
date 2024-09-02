311f43a7395f2b20aa0c1ea4d93cde0c9d2c9c68
Vladislav <44534111+Vladik356@users.noreply.github.com>
2024-09-02 22:25:09 +0300

    Update MtsByTests.java

--- src/test/java/Lesson_9/MtsByTests.java
+++ src/test/java/Lesson_9/MtsByTests.java
@@ -4,14 +4,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import pages.HomePage;
 import pages.PaymentPage;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MtsByTests {
 
@@ -28,6 +29,7 @@ public class MtsByTests {
 
         driver = new ChromeDriver(options);
         driver.get("https://www.mts.by/");
+
 
         homePage = new HomePage(driver);
         paymentPage = new PaymentPage(driver);
@@ -47,30 +49,18 @@ public class MtsByTests {
 
     @Test
     public void testPlaceholderTexts() {
-        homePage.openPaymentPage();  // Переход на страницу оплаты
-        assertEquals("Введите номер телефона", paymentPage.getPlaceholderText(paymentPage.phoneNumberInput), "Неправильный placeholder для 'Услуги связи'");
-        paymentPage.selectInternetOption();
-        assertEquals("Введите номер лицевого счета", paymentPage.getPlaceholderText(paymentPage.phoneNumberInput), "Неправильный placeholder для 'Домашний интернет'");
-        paymentPage.selectInstallmentOption();
-        assertEquals("Введите номер договора", paymentPage.getPlaceholderText(paymentPage.phoneNumberInput), "Неправильный placeholder для 'Рассрочка'");
-        paymentPage.selectDebtOption();
-        assertEquals("Введите номер договора", paymentPage.getPlaceholderText(paymentPage.phoneNumberInput), "Неправильный placeholder для 'Задолженность'");
+        homePage.clickMoreInfoLink();  // Переход на страницу оплаты
+        assertEquals("Введите номер телефона", paymentPage.getPlaceholderText(By.id("phone-number-input")), "Неправильный placeholder для 'Услуги связи'");
     }
 
     @Test
     public void testContinueButtonForServices() {
-        homePage.openPaymentPage();  // Переход на страницу оплаты
+        homePage.clickMoreInfoLink();  // Переход на страницу оплаты
         paymentPage.selectServiceOption();
         paymentPage.enterPhoneNumber("297777777");
         paymentPage.clickContinueButton();
 
-        // Проверка корректности отображения суммы, номера телефона и плейсхолдера для карты
+        // Проверка суммы
         assertEquals("10.00 BYN", paymentPage.getAmountText(), "Некорректная сумма");
-        assertEquals("297777777", paymentPage.getPhoneNumberDisplayText(), "Некорректный номер телефона");
-        assertEquals("Введите номер карты", paymentPage.getPlaceholderText(paymentPage.cardNumberPlaceholder), "Неправильный placeholder для карты");
-
-        // Проверка наличия иконок платёжных систем
-        assertTrue(paymentPage.isPaymentIconDisplayed(paymentPage.visaLogo), "Логотип Visa не отображается");
-        assertTrue(paymentPage.isPaymentIconDisplayed(paymentPage.masterCardLogo), "Логотип MasterCard не отображается");
     }
 }
